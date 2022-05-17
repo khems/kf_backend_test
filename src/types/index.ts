94c9fd6c2ba12baaa9f98b9312a366bac14d915e
Khem Sattaur <hello@khems.net>
2022-05-17 20:25:47 +0100

    Change appropriate types to interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,27 +1,27 @@
-type ErrorMessage = {
+interface ErrorMessage {
   message: string;
-};
+}
 
-type Outage = {
+interface Outage {
   id: string;
   begin: string;
   end: string;
-};
+}
 
 type SiteId = string;
 
-type SiteInfo = {
+interface SiteInfo {
   id: string;
   name: string;
   devices: Device[];
-};
+}
 
 type DeviceId = string;
 
-type Device = {
+interface Device {
   id: DeviceId;
   name: string;
-};
+}
 
 interface EnhancedOutage extends Outage {
   name: string;
